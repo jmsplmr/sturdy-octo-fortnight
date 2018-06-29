62d9c9aa02d5727151e75d5a6a78ff7cebb70268
James Palmer <jms.plmr@gmail.com>
2018-06-29 14:23:31 -0600

    Stubs and basics for a server for youtube

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var app = express();
+
+const PORT = process.env.PORT;
+
+const BASE_URL = process.env.ENV_URL;
+
+app.set("PORT", 5000)
+   .use(express.json())
+   .use(express.urlencoded({extended:true}))
+   .use(express.static(__dirname + "/public"))
+   .get("/video", getVideo)
+   .get("tags", getTags)
+   .post("/video", postVideo)
+   .listen(app.get("PORT"), function() {
+     console.log("Listening on port: " + app.get("PORT"));
+     console.log(PORT);
+     console.log(BASE_URL);
+   });
+
+function getVideo(req, res) {
+  console.log("Get video:");
+}
+
+function getTags(req, res) {
+  console.log("Get tags:");
+}
+
+function postVideo(req, res) {
+  console.log("Post video:");
+}
