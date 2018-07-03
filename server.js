da9e2ca5e4cbfff01f56c39bd2dd72e030f0878a
James Palmer <jms.plmr@gmail.com>
2018-07-03 13:01:03 -0600

    Get a course

--- server.js
+++ server.js
@@ -1,46 +1,125 @@
 var express = require("express");
 var app = express();
-const { Pool } = require("pg");
+const {
+  Pool
+} = require("pg");
 
 const connectionString = process.env.DATABASE_URL || "postgres://user:password@localhost:5432/database";
-const pool = new Pool({connectionString: connectionString});
+const pool = new Pool({
+  connectionString: connectionString
+});
 
 
 const PORT = process.env.PORT || 8000;
 const BASE_URL = process.env.ENV_URL || "localhost";
 
 app.set("PORT", PORT)
-   .use(express.json())
-   .use(express.urlencoded({extended:true}))
-   .use(express.static(__dirname + "/public"))
-   .get("/video", getVideo)
-   .get("/tags", getTags)
-   .get("/courses", getCourses)
-   .post("/video", postVideo)
-   .listen(app.get("PORT"), function() {
-     console.log("Listening on port: " + app.get("PORT"));
-     console.log(PORT);
-     console.log(BASE_URL);
-   });
+  .use(express.json())
+  .use(express.urlencoded({
+    extended: true
+  }))
+  .use(express.static(__dirname + "/public"))
+  .get("/courses/:id?", getCourses)
+  .get("/course/:id", getCourse)
+  .get()
+  .get()
+  .post("/authenticate")
+  .listen(app.get("PORT"), function () {
+    console.log("Listening on port: " + app.get("PORT"));
+    console.log(PORT);
+    console.log(BASE_URL);
+  });
 
-function getCourses(req, res) {
-  getCoursesFromDB(function(error, result) {
-    if (error || result == null || result.length < 1) {
-      console.log("Something is wrong");
-      res.status(500).json({success:false, data:error});      
+
+function getCourseFromDBbyId(id, callback) {
+  var sql = "SELECT name, street_address, city, state, zip, phone, contact FROM courses WHERE id = $1::int";
+  var params = [id];
+
+  pool.query(sql, params, function(err, result) {
+    if(err){
+      console.log("Server error");
+      console.error(err);
+      callback(err,null);
     }
+    console.log("Got results: "+ result);
+    callback(null, result.rows);
+  });
+}
 
+function getCourse(req, res) {
+  var id = req.query.id;
+  console.log("Get course" + id);
+
+  getCourseFromDBbyId(id, function(err, result) {
+    if (err || result == null || result.length < 1) {
+      console.log("Something is wrong");
+      res.status(500).json({
+        success: false,
+        data: error
+      });
+    }
     console.log("Back with result:" + result);
     res.json(result);
   });
 }
 
+function getCourses(req, res) {
+
+  var id = req.params.id || null;
+
+  if (id) {
+    getUsersRatedCoursesFromDB(id, function(err, result) {
+      if (err || result == null || result.length < 1) {
+        console.log("Something is wrong");
+        res.status(500).json({
+          success: false,
+          data: err
+        });
+      }
+      console.log("Back with result:" + result);
+      res.json(result);
+    });
+  } else {
+    getCoursesFromDB(function (err, result) {
+      if (err || result == null || result.length < 1) {
+        console.log("Something is wrong");
+        res.status(500).json({
+          success: false,
+          data: err
+        });
+      }
+      console.log("Back with result:" + result);
+      res.json(result);
+    });
+  }
+}
+
+function getUsersRatedCoursesFromDB(id, callback) {
+  console.log("GetUsersRatedCourseFromDB, User:" + id);
+  
+  var params = [id];
+  var sql = "SELECT courses.id, name, street_address AS address, city, state, rating FROM courses INNER JOIN course_rating rating2 on courses.id = rating2.course_id WHERE rating2.user_id = $1::int";
+
+  pool.query(sql, params, function(err, result) {
+    if(err){
+      console.log("Server error");
+      console.error(err);
+      callback(err,null);
+    }
+    console.log("Got results: "+ result);
+    callback(null, result.rows);
+  });
+}
+
 function getCoursesFromDB(callback) {
+
+  console.log("Get courses from DB");
+  
   var sql = "SELECT name, street_address, city, state, zip, phone, contact FROM courses";
 
-  pool.query(sql, function(err, result){
+  pool.query(sql, function (err, result) {
     if (err) {
-      console.log("Server console");
+      console.log("Server error");
       console.error(err);
       callback(err, null);
     }
@@ -49,17 +128,4 @@ function getCoursesFromDB(callback) {
 
     callback(null, result.rows);
   });
-}
-
-function getVideo(req, res) {
-  console.log("Get video:");
-}
-
-function getTags(req, res) {
-  console.log("Get tags:");
-  //console.log(process.env);
-}
-
-function postVideo(req, res) {
-  console.log("Post video:");
 }
